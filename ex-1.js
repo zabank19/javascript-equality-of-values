636838545a919cf3f8a022df53d307e9230210de
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-27 11:05:57 +0700

    Update ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,5 @@
+// Exercise #1: Strict and Same
+// Start commenting here
 Object.is(10, 20);
 Object.is(0, -0);
 Object.is(NaN, 100);
@@ -6,6 +8,7 @@ Object.is({ name: "John", age: 30 }, { name: "John", age: 30 });
 Object.is(null, undefined);
 
 10 === 20;
+10 === "10"
 3.14 !== 3.14;
 "TechUp" !== "TechUp";
 NaN === NaN;
