980962006cd65868ae08e9aacba60b53f7100581
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-27 11:24:15 +0700

    Update ex-3.js

--- ex-3.js
+++ ex-3.js
@@ -2,11 +2,13 @@ function isPlainObject(obj) {
   // Start coding here
 }
 
+
+// Example case
 let result1 = isPlainObject({ name: "John" });
-console.log(result1); //true
-
 let result2 = isPlainObject(null);
-console.log(result2); // false
-
 let result3 = isPlainObject(["apples", "oranges"]);
+
+// ผลลัพธ์ควรที่ได้จาก Example case
+console.log(result1); //true
+console.log(result2); // false
 console.log(result3); // false
